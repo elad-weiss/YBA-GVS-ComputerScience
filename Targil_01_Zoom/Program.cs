1b5487cfe3b0ce8aa3b3f22958e202470c91fc01
elad-weiss <elad_Weiss@hotmail.com>
2021-10-03 22:18:20 +0300

    first class example code

---
+++ Targil_01_Zoom/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Targil_01_Zoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************");
+            Console.WriteLine("*   Hello World   *");
+            Console.WriteLine("*******************");
+            Console.ReadKey();
+        }
+    }
+}
