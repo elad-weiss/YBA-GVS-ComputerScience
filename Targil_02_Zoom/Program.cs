ba5260ca533bbd602209a99ac51e83da1acb977a
elad-weiss <elad_Weiss@hotmail.com>
2021-10-05 20:07:25 +0300

    added code example from class #4

---
+++ Targil_02_Zoom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Targil_02_Zoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int sum;
+            Console.WriteLine("Enter 2 numbers");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, sum);
+            Console.ReadKey();
+        }
+    }
+}
