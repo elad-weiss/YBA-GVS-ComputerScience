447b10fd2a13125697bf9f19a1bb6e4fb3b46f22
elad-weiss <elad_Weiss@hotmail.com>
2021-10-12 16:50:51 +0300

    added targil #3

---
+++ Targil_03_Zoom/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Targil_03_Zoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, sum, diff, prod, div;
+            Console.WriteLine("Enter 2 integers");
+            num1 = float.Parse(Console.ReadLine());
+            num2 = float.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            diff = num1 - num2;
+            prod = num1 * num2;
+            div = num1 / num2;
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, sum);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, diff);
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, prod);
+            Console.WriteLine("{0} : {1} = {2}", num1, num2, div);
+
+            Console.ReadKey();
+        }
+    }
+}
